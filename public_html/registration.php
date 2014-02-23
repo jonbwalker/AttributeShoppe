9baf4cfda8bdb1707f2459b64adbf0c364ce84a0
jonwalker <jonathan.b.walker0@gmail.com>
2014-02-22 18:53:06 -0600

    added registration form, datepicker resource files

--- public_html/registration.php
+++ public_html/registration.php
@@ -32,6 +32,47 @@ require_once(TEMPLATES_PATH . "/header.php");
 ?>
 
 <div class="container">
+    <div class="row">
+        <div class="box">
+            <div class="col-lg-12">
+                <hr>
+                <h2 class="intro-text text-center">Registration <strong>form</strong>
+                </h2>
+                <hr>
+                <p>Register</p>
+
+                <form role="form">
+                    <div class="row">
+                        <div class="form-group col-lg-4">
+                            <label>First Name</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group col-lg-4">
+                            <label>Last Name</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group col-lg-4">
+                            <label>Email Address</label>
+                            <input type="email" class="form-control">
+                        </div>
+                        <div class="form-group col-lg-4">
+                            <label>Phone Number</label>
+                            <input type="tel" class="form-control">
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="form-group col-lg-12">
+                            <label>Message</label>
+                            <textarea class="form-control" rows="6"></textarea>
+                        </div>
+                        <div class="form-group col-lg-12">
+                            <input type="hidden" name="save" value="contact">
+                            <button type="submit" class="btn btn-default">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 </div>
 <!-- /.container -->
