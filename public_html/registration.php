c722ef55abb9d57b393e8f45e7c391c575cff210
jonwalker <jonathan.b.walker0@gmail.com>
2014-02-27 19:18:08 -0600

    moved registration form processing to its own file

--- public_html/registration.php
+++ public_html/registration.php
@@ -1,107 +1,4 @@
 <?php
-// define variables and initialize with empty values
-$firstnameErr = $lastnameErr = $emailErr = $phoneErr = $dobErr = $genderErr =  $usernameErr = $passwordErr = "";
-$firstname = $lastname = $email = $phone = $dob = $gender = $username = $password =  "";
-$error = array();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["firstname"])) {
-        $firstnameErr = "Please enter your first name";
-        array_push($error, $firstnameErr);
-    }
-    else {
-        $firstname = sanitize($_POST["firstname"]);
-    }
-
-    if (empty($_POST["lastname"])) {
-        $lastnameErr = "Please enter your last name";
-        array_push($error, $lastnameErr);
-    }
-    else {
-        $lastname = sanitize($_POST["lastname"]);
-    }
-
-    if (empty($_POST["email"]))  {
-        $emailErr = "Please enter a valid email";
-        array_push($error, $emailErr);
-    }
-    else {
-        $email = sanitize($_POST["email"]);
-    }
-
-    if (empty($_POST["phone"])) {
-        $phoneErr = "Please enter a valid Us phone number";
-        array_push($error, $phoneErr);
-    }
-    else {
-        $phone = sanitize($_POST["phone"]);
-    }
-
-    if (empty($_POST["dob"])) {
-        $dobErr = "Please enter a valid date";
-        array_push($error, $dobErr);
-    }
-    else {
-        $dob = sanitize($_POST["dob"]);
-    }
-
-    if (!isset($_POST["gender"])) {
-        $genderErr = "Please select a gender";
-        array_push($error, $genderErr);
-    }
-    else {
-        $gender = $_POST["gender"];
-    }
-
-    if (empty($_POST["username"])) {
-        $usernameErr = "Please enter a username";
-        array_push($error, $usernameErr);
-    }
-    else {
-        $username = sanitize($_POST["username"]);
-    }
-
-    if (empty($_POST["password"])) {
-        $passwordErr = "Please enter a password";
-        array_push($error, $passwordErr);
-    }
-    else {
-        $password = sanitize($_POST["password"]);
-    }
-
-    if(empty($error)) {
-     write();
-        header("Location: success.php");
-    }else{
-        $formError = "Errors in form";
-    }
-}
-
-function sanitize($input)
-{
-    $input = trim($input);
-    $input = stripslashes($input);
-    $input = htmlspecialchars($input);
-    return $input;
-}
-
-function write(){
-    $data = 'User: ' . $_POST['firstname'] . ' '
-        . $_POST['lastname'] . ' '
-        . $_POST['email'] . ' '
-        . $_POST['phone'] . ' '
-        . $_POST['dob'] . ' '
-        . $_POST['gender'] . ' '
-        . $_POST['username'] . ' '
-        . $_POST['password'] ."\n";
-    $ret = file_put_contents('/Users/jon/Desktop/registration.txt', $data, FILE_APPEND | LOCK_EX);
-    if($ret === false) {
-        die('There was an error writing this file');
-    }
-    else {
-        echo "$ret bytes written to file";
-    }
-}
 
 ?>
 
@@ -137,6 +34,8 @@ function write(){
 require_once("../resources/config.php");
 // include header navigation bar
 require_once(TEMPLATES_PATH . "/header.php");
+// include form processing
+include("../resources/library/phpscripts/registration-form.php")
 ?>
 
 <div class="container" ng-app="">
--- resources/library/phpscripts/registration-form.php
+++ resources/library/phpscripts/registration-form.php
@@ -1,26 +1,29 @@
 <?php
 // define variables and initialize with empty values
-$firstNameErr = $lastNameErr = $emailErr = $phoneErr = $dobErr = $genderErr =  $usernameErr = $passwordErr = "";
-$firstName = $lastName = $email = $phone = $dob = $gender = $username = $password =  "";
-$favFruit = array();
+$firstnameErr = $lastnameErr = $emailErr = $phoneErr = $dobErr = $genderErr =  $usernameErr = $passwordErr = "";
+$firstname = $lastname = $email = $phone = $dob = $gender = $username = $password =  "";
+$error = array();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["firstName"])) {
-        $firstNameErr = "Please enter your first name";
+    if (empty($_POST["firstname"])) {
+        $firstnameErr = "Please enter your first name";
+        array_push($error, $firstnameErr);
     }
     else {
-        $firstName = sanitize($_POST["firstName"]);
+        $firstname = sanitize($_POST["firstname"]);
     }
 
-    if (empty($_POST["lastName"])) {
-        $lastNameErr = "Please enter your last name";
+    if (empty($_POST["lastname"])) {
+        $lastnameErr = "Please enter your last name";
+        array_push($error, $lastnameErr);
     }
     else {
-        $lastName = sanitize($_POST["lastName"]);
+        $lastname = sanitize($_POST["lastname"]);
     }
 
     if (empty($_POST["email"]))  {
         $emailErr = "Please enter a valid email";
+        array_push($error, $emailErr);
     }
     else {
         $email = sanitize($_POST["email"]);
@@ -28,6 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (empty($_POST["phone"])) {
         $phoneErr = "Please enter a valid Us phone number";
+        array_push($error, $phoneErr);
     }
     else {
         $phone = sanitize($_POST["phone"]);
@@ -35,6 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (empty($_POST["dob"])) {
         $dobErr = "Please enter a valid date";
+        array_push($error, $dobErr);
     }
     else {
         $dob = sanitize($_POST["dob"]);
@@ -42,6 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (!isset($_POST["gender"])) {
         $genderErr = "Please select a gender";
+        array_push($error, $genderErr);
     }
     else {
         $gender = $_POST["gender"];
@@ -49,17 +55,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (empty($_POST["username"])) {
         $usernameErr = "Please enter a username";
+        array_push($error, $usernameErr);
     }
     else {
         $username = sanitize($_POST["username"]);
     }
 
-
     if (empty($_POST["password"])) {
         $passwordErr = "Please enter a password";
+        array_push($error, $passwordErr);
     }
     else {
         $password = sanitize($_POST["password"]);
+    }
+
+    if(empty($error)) {
+        write();
+        header("Location: success.php");
+    }else{
+        $formError = "Errors in form";
     }
 }
 
@@ -71,6 +85,24 @@ function sanitize($input)
     return $input;
 }
 
+function write(){
+    $data = 'User: ' . $_POST['firstname'] . ' '
+        . $_POST['lastname'] . ' '
+        . $_POST['email'] . ' '
+        . $_POST['phone'] . ' '
+        . $_POST['dob'] . ' '
+        . $_POST['gender'] . ' '
+        . $_POST['username'] . ' '
+        . $_POST['password'] ."\n";
+    $ret = file_put_contents('/Users/jon/Desktop/registration.txt', $data, FILE_APPEND | LOCK_EX);
+    if($ret === false) {
+        die('There was an error writing this file');
+    }
+    else {
+        echo "$ret bytes written to file";
+    }
+}
+
 //function saveToFile()  {
 //$fp = fopen(”formdata.txt”, “a”);
 //$savestring = $name . “,” . $email . “n”;
@@ -79,19 +111,4 @@ function sanitize($input)
 //echo “<h1>You data has been saved in a text file!</h1>”;
 //}
 
-function save(){
-    if(isset($_POST['field1']) && isset($_POST['field2'])) {
-        $data = $_POST['field1'] . '-' . $_POST['field2'] . "\n";
-        $ret = file_put_contents('/tmp/mydata.txt', $data, FILE_APPEND | LOCK_EX);
-        if($ret === false) {
-            die('There was an error writing this file');
-        }
-        else {
-            echo "$ret bytes written to file";
-        }
-    }
-    else {
-        die('no post data to process');
-    }
-}
 ?>
