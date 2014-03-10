079078b37358dc4d29b428c84b9ec94435b93285
jonwalker <jonathan.b.walker0@gmail.com>
2014-03-09 19:13:52 -0500

    added user login using database

--- resources/library/phpscripts/process-login.php
+++ resources/library/phpscripts/process-login.php
@@ -2,9 +2,28 @@
 // Create connection
 $con=mysqli_connect("127.0.0.1","attrib","password","attribute_shoppe");
 
-// Check connection
-if (mysqli_connect_errno())
-{
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if (isset($_POST['submit'])){
 }
+    $user = trim ($_POST['username']);
+    $passwd = trim ($_POST['password']);
+    $dbcn = new mysqli
+
+    ("127.0.0.1","attrib","password","attribute_shoppe");
+
+    // Check connection
+    if(mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit;
+    }
+
+    $sql = "SELECT username, password FROM USER ";
+
+    $sql = $sql . "WHERE username='$user' AND pass=sha1('$passwd');";
+
+    $result = $dbcn->query( $sql );
+
+    if(!$result){
+        echo( "Unable to query database at this time." );
+        exit();
+    }
 ?>
