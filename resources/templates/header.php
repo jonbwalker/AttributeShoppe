f826933a0721b843edc8c05292c075cf6ead7f62
jonwalker <jonathan.b.walker0@gmail.com>
2014-03-19 19:59:20 -0500

    dropdown on navbar

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -28,7 +28,18 @@
 <!--                </li>-->
                 <li><a href="contact.php">Contact</a>
                 </li>
-
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
             </ul>
         </div>
 
