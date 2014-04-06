be5febcbecab81f99f95093bca6e6059dbef3c28
jonwalker <jonathan.b.walker0@gmail.com>
2014-04-05 22:28:45 -0500

    product page

--- public_html/categories/virtues.php
+++ public_html/categories/virtues.php
@@ -1,3 +1,10 @@
+<?php
+//include("../../resources/library/phpscripts/products/update.php");
+$conn = new mysqli('localhost', 'attrib', 'password', 'attribute_shoppe');
+$sql = "SELECT * FROM PRODUCT";
+$result = $conn->query($sql);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -44,14 +51,34 @@
                 <h2 class="intro-text text-center">Virtues <strong>attribute shoppe</strong>
                 </h2>
                 <hr>
-
+                <?php
+                foreach($conn->query($sql) as $row) {
+                echo '<div class="row">';
+                    echo '<div class="col col-lg-4 col-sm-6">';
+                        echo '<div class="thumbnail">';
+                             echo '<a href="/resources/library/phpscripts/products/show.php?id='.$row['ID'].'" title="courage">';
+                                 echo '<img alt="courage" itemprop="image" src="resources/library/img/COURAGE.png">';
+                             echo '</a>';
+                             echo '<div class="caption">';
+                                 echo '<h5 class="ellipsis">';
+                                     echo '<a href="/resources/library/phpscripts/products/show.php?id='.$row['ID'].'" itemprop="url" title="Courage">';
+                                        echo '<span itemprop="brand">' .$row['NAME'].'</span>';
+                                        echo '<span itemprop="name"></span>';
+                                     echo '</a>';
+                                 echo '</h5>';
+                                 echo '<div itemprop="offers" itemscope itemtype="http://schema.org/Offer">';
+                                     echo '<span class="label label-danger" itemprop="price">' .$row['PRICE'].'</span>';
+                            echo '</div>';
+                        echo '</div>';
+                     echo '</div>';
+                echo '</div>';
+                }?>
                 <!-- product -->
                 <div class="row">
                     <div class="col col-lg-4 col-sm-6">
-                        <div class="thumbnail" itemscope itemtype="http://schema.org/Product">
-                            <a href="public_html/courage.php" title="courage">
-                                <img alt="courage" itemprop="image"
-                                     src="resources/library/img/COURAGE.png">
+                        <div class="thumbnail">
+                            <a href="/resources/library/phpscripts/products/show.php?id='.$row['ID'].'" title="courage">
+                                <img alt="courage" itemprop="image" src="resources/library/img/COURAGE.png">
                             </a>
                             <div class="caption">
                                 <h5 class="ellipsis">
