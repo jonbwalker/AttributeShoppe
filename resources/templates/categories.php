e64df3be7a53572a815bc675ed6c9ba99e865c24
jonwalker <jonathan.b.walker0@gmail.com>
2014-04-05 21:42:34 -0500

    category list from database

--- resources/templates/categories.php
+++ resources/templates/categories.php
@@ -1,19 +1,28 @@
+<?php
+$conn = new mysqli('localhost', 'attrib', 'password', 'attribute_shoppe');
+$category = "SELECT * FROM CATEGORY";
+$result = $conn->query($category);
+
+
+?>
+
 <div class="panel">
     <div class="panel-heading">
         <h4>Categories</h4>
     </div>
     <div class="list-group list-group-flush">
-        <a class="list-group-item" href="<?php echo BASE_URL; ?>/categories/virtues.php">
-            <span class="badge">2</span>
-            Virtues
-        </a>
-        <a class="list-group-item" href="<?php echo BASE_URL; ?>/categories/attributes.php">
-            <span class="badge">2</span>
-            Attributes
-        </a>
-        <a class="list-group-item" href="<?php echo BASE_URL; ?>/categories/enhancements.php">
-            <span class="badge">0</span>
-            Enhancements
-        </a>
+        <?php
+
+        foreach ($conn->query($category) as $row) {
+            $id = $row['ID'];
+            $product = "SELECT COUNT(*) FROM PRODUCT WHERE CATEGORY_ID = '$id'";
+            $result = $conn->query($product);
+            $count = $result->fetch_row();
+
+            echo '<a class="list-group-item" href="' . BASE_URL . '/categories/'.$row['NAME'].'.php">';
+            echo '<span class="badge">'.$count[0].'</span>';
+            echo $row['NAME'];
+            echo '</a>';
+        }?>
     </div>
 </div>
