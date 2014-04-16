9abb0266fb97d7ef2fb304c27c80fc3cfabdebe6
jonwalker <jonathan.b.walker0@gmail.com>
2014-04-15 19:07:41 -0500

    add to cart button

--- resources/library/phpscripts/products/product.php
+++ resources/library/phpscripts/products/product.php
@@ -27,6 +27,7 @@ foreach($conn->query($sql) as $row) {
     echo '</h5>';
     echo '<div itemprop="offers" itemscope itemtype="http://schema.org/Offer">';
     echo '<span class="label label-danger" itemprop="price">' .$row['PRICE'].'</span>';
+    echo '<input class="btn btn-default add-to-cart" type="button" value="Add to Cart" onclick="" />';
     echo '</div>';
     echo '</div>';
     echo '</div>';
